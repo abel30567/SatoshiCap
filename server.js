d955f9210137150b421aac2927fc264a2056e750
abel30567 <claudio@imarkett.com>
2018-02-03 19:54:59 -0500

    nominer

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ var express = require('express');
 var request = require('request');
 var ejs = require('ejs');
 var ejsMate = require('ejs-mate'); 
+var bodyParser = require('body-parser');
 
+var publishableKey = "pk_test_7E9MXx8EBKLczZ8cbimXrxOe";
+var secretKey = "sk_test_gDk4NwJIibaNmXJ2K4tWKYYm";
+
+var stripe = require("stripe")(secretKey);
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
+
+app.use(require("body-parser").urlencoded({extended: true}));
+
 // 
 app.get('/', function(req, res) {
     request('https://api.coinmarketcap.com/v1/ticker/', function(error, response, body) {
@@ -23,11 +31,15 @@ app.get('/', function(req, res) {
         });
     });
 });    
-app.get('/miner', function(req, res) {
-    res.render('Miner.ejs');
+app.get('/buy', function(req, res) {
+    request('https://api.coinmarketcap.com/v1/ticker/', function(error, response, body) {
+        var chart = JSON.parse(body);
+        var BTCprice = Number(chart[0].price_usd).toFixed(2);
+        res.render('Purchase.ejs', { keyPublishable: publishableKey, BTCprice: BTCprice });
+    });
 });
 
-var PORT = 80;
+var PORT = 3000;
 
 app.listen(PORT, function(err, res) {
     if (err) {
